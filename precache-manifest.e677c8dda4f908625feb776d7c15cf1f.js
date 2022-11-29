c6511e83b8ec656296649f1870a27fd992012b77
luis2605 <89785699+luis2605@users.noreply.github.com>
2022-11-29 11:41:47 +0100

    Updates

---
+++ precache-manifest.e677c8dda4f908625feb776d7c15cf1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19d1e769b120e792f388",
+    "url": "/github.io/robotFriends/static/css/main.0f786d66.chunk.css"
+  },
+  {
+    "revision": "19d1e769b120e792f388",
+    "url": "/github.io/robotFriends/static/js/main.19d1e769.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/github.io/robotFriends/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "7c2e3ceb3117c517617c",
+    "url": "/github.io/robotFriends/static/css/2.2aa54b11.chunk.css"
+  },
+  {
+    "revision": "7c2e3ceb3117c517617c",
+    "url": "/github.io/robotFriends/static/js/2.7c2e3ceb.chunk.js"
+  },
+  {
+    "revision": "5c64be8d4905eece925d",
+    "url": "/github.io/robotFriends/static/js/runtime~main.5c64be8d.js"
+  },
+  {
+    "revision": "a630f0442ed36b1dcfd70b2219e07047",
+    "url": "/github.io/robotFriends/index.html"
+  }
+];
