b45afeb161b0986a9fe00b2bf6d1786f569e5fb1
luis2605 <89785699+luis2605@users.noreply.github.com>
2022-11-29 08:20:36 +0100

    save before audit fix

--- robot-friends/src/Searchbox.js
+++ robot-friends/src/Searchbox.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./searchbox.css";
 
 const Searchbox = ({ onSearch }) => {
   return (
     <div className="pa2">
       <input
-        className="pa3 ba b--green bg-lightest-blue br-pill "
+        className=" bloob pa3 ba b--green bg-lightest-blue br-pill "
         type="search"
         placeholder="Search Robots"
         onChange={onSearch}
